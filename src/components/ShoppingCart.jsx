a36690c47a506d1bfa00d7350a57bd8b78256448
seandesiree <sean.desiree@gmail.comgit config --global user.email sean.desiree@gmail.comgit versioncdgit config --global user.email sean.desiree@gmail.com>
2025-01-05 13:33:35 -0500

    update

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeFromCart, clearCart } from './components/store';
+import { removeFromCart, clearCart } from './Store';
 
 
 const ShoppingCart = () => {
